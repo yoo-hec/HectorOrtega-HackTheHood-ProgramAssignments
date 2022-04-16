9dd233d9a7f39bff9c826387c788e4972a46b19d
yoo-hec <103329355+yoo-hec@users.noreply.github.com>
2022-04-16 12:23:59 -0700

    Update fundementals_practice.py

--- fundementals_practice.py
+++ fundementals_practice.py
@@ -7,3 +7,12 @@ jersey_number= "none"
 car_name= "toyota"
 x= 50
 first_name= "Hector"
+print(name)
+print(age)
+print(years_at_hth)
+print(plays_basketball)
+print(jersey_number)
+print(car_name)
+print(x)
+print(first_name)
+
